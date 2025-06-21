b6a5c053998edcf2fcebd35f5885bbf0e2a4787f
Raihan Rony <91003324+Moskov-1@users.noreply.github.com>
2025-06-21 22:37:46 +0600

    trying to make a automated super admin

--- build.sh
+++ build.sh
@@ -12,3 +12,16 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 
 python manage.py migrate
+
+
+# Create superuser from env vars
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+email = "${DJANGO_SUPERUSER_EMAIL}"
+username = "${DJANGO_SUPERUSER_USERNAME}"
+password = "${DJANGO_SUPERUSER_PASSWORD}"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+END
